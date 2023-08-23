156eb6465de4500532a254253ba51e880d2573b9
giuliabruno05 <giuliabruno48@gmail.com>
2023-08-23 13:11:19 +0200

    add a controller and create a route for cuminication with frontend

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function projectApi(){
+        return response() -> json([
+            'name' => 'prova'
+        ]);
+    }
+    public function projectIndex(){
+        $projects = Project :: with('type') -> paginate(5);
+   
+        return response() -> json([
+           'projects' => $projects
+        ]);
+   }
 }
